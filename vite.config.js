7b11a3bda9045cf6a013d04ab73d74f07c0d6965
Yoshio Alberto Valles Marquez <yvalles84@gmail.com>
2024-07-18 21:12:30 -0700

    update viteconfig and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+      {
+        find: '@features',
+        replacement: fileURLToPath(new URL('./src/features', import.meta.url)),
+      },
+      {
+        find: '@apis',
+        replacement: fileURLToPath(new URL('./src/common/apis', import.meta.url)),
+      },
+      {
+        find: '@models',
+        replacement: fileURLToPath(new URL('./src/common/models', import.meta.url)),
+      },
+      {
+        find: '@constants',
+        replacement: fileURLToPath(new URL('./src/common/constants', import.meta.url)),
+      },
+    ],
+  },
 })
