8104b919e7b640360e6e09c00daec82b0c35e778
Yoshio Alberto Valles Marquez <yvalles84@gmail.com>
2024-07-18 21:12:54 -0700

    added model state code

---
+++ src/common/constants/modelStateCode.js
@@ -0,0 +1,3 @@
+export const CREATE = 1 // Representa el estado de creación.
+export const UPDATE = 2 // Representa el estado de actualizacion.
+export const DELETE = 3 // Representa el estado de eliminación.
